c190a6d8d0ea31edf5b8463dd9aa4a3731cb638c
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-22 08:50:25 +0300

    Added Arkanoid_13

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -4,8 +4,53 @@ package com.javarush.task.task24.task2413;
  * Created by arseniy.barsegyan on 21.04.2017.
  */
 public class Ball extends BaseObject {
+    private double speed;
+    private double direction;
+    private double dx;
+    private double dy;
+    private boolean isFrozen;
+
     public Ball(double x, double y, double radius) {
         super(x, y, radius);
+    }
+
+    public Ball(double x, double y, double speed, double direction) {
+        super(x, y, 1);
+        this.speed = speed;
+        this.direction = direction;
+        this.isFrozen = true;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public double getDirection() {
+        return direction;
+    }
+
+    public void setDirection(double direction) {
+        this.direction = direction;
+    }
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
     }
 
     @Override
