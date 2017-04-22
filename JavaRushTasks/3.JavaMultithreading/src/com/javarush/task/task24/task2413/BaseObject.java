6b4ef32d22a4e8a92acc544e7870f770d8295e12
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-23 00:16:25 +0300

    Finished and added all task Arkanoid

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -1,14 +1,16 @@
 package com.javarush.task.task24.task2413;
 
 /**
- * Created by arseniy.barsegyan on 21.04.2017.
+ * Базовый класс для всех объектов игры.
  */
 public abstract class BaseObject {
+    //координаты
     protected double x;
     protected double y;
+    //радиус объекта
     protected double radius;
 
-    public BaseObject(double x, double y, double radius) {
+    protected BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -38,13 +40,34 @@ public abstract class BaseObject {
         this.radius = radius;
     }
 
-    public abstract void draw(Canvas canvas);
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    abstract void draw(Canvas canvas);
 
-    public abstract void move();
+    /**
+     * Двигаем себя на один ход.
+     */
+    abstract void move();
 
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
     boolean isIntersec(BaseObject o) {
-        double distanceBetweenObjects = Math.sqrt((o.getX()-this.getX())*(o.getX()-this.getX()) +
-                (o.getY() - this.getY())*(o.getY() - this.getY()));
-        return distanceBetweenObjects <= Math.max(o.getRadius(), this.getRadius());
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
     }
 }
