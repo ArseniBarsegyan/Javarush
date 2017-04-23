fb8c88b77a9fa1e936002111edbc3b0b212f3b17
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-23 18:45:00 +0300

    Added task2401

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarker.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task24.task2401;
+
+/**
+ * Created by Administrator on 23.04.2017.
+ */
+public interface SelfInterfaceMarker {
+}
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task24.task2401;
+
+import com.javarush.task.task24.task2401.SelfInterfaceMarker;
+
+/**
+ * Created by Administrator on 23.04.2017.
+ */
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker {
+    public void getInfo() {
+
+    }
+
+    public void getDescription() {
+
+    }
+}
