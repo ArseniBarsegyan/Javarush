95b10b548611fefd654f4286b76cabc87a67e2aa
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-04-28 13:54:14 +0300

    Added MVC_01 task.

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by arseniy.barsegyan on 28.04.2017.
+ */
+public class FakeModel implements Model {
+    private ModelData modelData = new ModelData();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList();
+        users.add(new User("1", 1,1));
+        users.add(new User("2", 2,2));
+        users.add(new User("3", 3,3));
+        this.modelData.setUsers(users);
+    }
+}
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/Model.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task36.task3608.model;
+
+/**
+ * Created by arseniy.barsegyan on 28.04.2017.
+ */
+public interface Model {
+    ModelData getModelData();
+    void loadUsers();
+}
