26c4b0eebaa8bb9c367f7d84b55808700960d06d
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-06 16:31:56 +0300

    Added task2202

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task22/task2202/Solution.java
@@ -1,47 +1,43 @@
 package com.javarush.task.task22.task2202;
 
-/* 
+import java.util.regex.Pattern;
+
+/*
 Найти подстроку
 */
 public class Solution {
     public static void main(String[] args) {
         System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java."));
-        System.out.println(getPartOfString("JavaRush - лучший сервис обучения."));
+        //System.out.println(getPartOfString("JavaRush-лучший сервис обучения Java."));
+        System.out.println(getPartOfString("JavaRush - лучший сервис обучения Java который учит Java."));
+        System.out.println(getPartOfString("JavaRush-лучшийсервис обучения Java который учит Java."));
     }
 
-    public static String getPartOfString(String string) throws TooShortStringException {
-        try {
-            int countSpaceIndex = 0;
-            int fistSpaceIndex = 0;
-            boolean firstFlag = true;
-            int fifthSpaceIndex = 0;
-            boolean fifthFlag = true;
-
-            for (int i = 0; i < string.length(); i++) {
-
-                if (string.charAt(i) == ' ') {
-                    countSpaceIndex++;
-                }
-
-                if (countSpaceIndex == 1 && firstFlag) {
-                    fistSpaceIndex = i;
-                    firstFlag = false;
-                }
-
-                if (countSpaceIndex == 5 && fifthFlag) {
-                    fifthSpaceIndex = i;
-                    fifthFlag = false;
-                }
-
-                if (string.charAt(i) == '.' && countSpaceIndex == 4) {
-                    fifthSpaceIndex = i;
-                    fifthFlag = false;
-                }
-            }
-            return string.substring(fistSpaceIndex + 1, fifthSpaceIndex);
-        } catch (Exception e) {
+    public static String getPartOfString(String string) {
+        if (string == null) {
             throw new TooShortStringException();
         }
+        String resultString = "";
+        String[] splitted = string.split(" ");
+        if (splitted.length < 5) {
+            throw new TooShortStringException();
+        } else {
+            int fifthSpaceCount = 0;
+            int spaceCount = 0;
+            for (int i = 0; i < string.length(); i++) {
+                if (string.charAt(i) == ' ') {
+                    spaceCount++;
+                    if (spaceCount == 5) {
+                        fifthSpaceCount = i;
+                        resultString = string.substring(string.indexOf(" ") + 1, fifthSpaceCount);
+                    }
+                }
+            }
+            if (spaceCount < 5) {
+                resultString = string.substring(string.indexOf(" ") + 1);
+            }
+        }
+        return resultString;
     }
 
     public static class TooShortStringException extends RuntimeException {
