35d67754bfc48c6b66dc7ad30e2c7e222f8f659d
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-15 16:00:40 +0300

    Added Refactoring_09 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -4,25 +4,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class Human implements Alive{
+public class Human implements Alive {
     private List<Human> children = new ArrayList<>();
     private static int nextId = 0;
     private int id;
     protected int age;
     protected String name;
+    private BloodGroup bloodGroup;
 
-    protected int[] size;
+    protected Size size;
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
 
     public Human(String name, int age) {
         this.name = name;
         this.age = age;
         this.id = nextId;
         nextId++;
+    }
+
+    public class Size {
+        public int height;
+        public int weight;
     }
 
     public List<Human> getChildren() {
@@ -53,12 +55,20 @@ public class Human implements Alive{
         children.remove(human);
     }
 
+    public BloodGroup getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
     public int getId() {
         return id;
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
 
     @Override
