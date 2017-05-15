056b805aff91d3da1a3e83ab0e7f8f1ed4030214
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-05-15 18:26:09 +0300

    Added Refactoring_12 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Sedan.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Sedan.java
@@ -4,7 +4,14 @@ package com.javarush.task.task29.task2909.car;
  * Created by arseniy.barsegyan on 15.05.2017.
  */
 public class Sedan extends Car {
+    private final int MAX_SEDAN_SPEED = 120;
+
     public Sedan(int numberOfPassengers) {
         super(1, numberOfPassengers);
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return MAX_SEDAN_SPEED;
     }
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Truck.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/car/Truck.java
@@ -4,7 +4,14 @@ package com.javarush.task.task29.task2909.car;
  * Created by arseniy.barsegyan on 15.05.2017.
  */
 public class Truck extends Car {
+    private final int MAX_TRUCK_SPEED = 80;
+
     public Truck(int numberOfPassengers) {
         super(0, numberOfPassengers);
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return MAX_TRUCK_SPEED;
     }
 }
