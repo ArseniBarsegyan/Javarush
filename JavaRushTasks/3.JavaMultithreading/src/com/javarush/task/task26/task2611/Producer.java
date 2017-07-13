d8f2823f6d14a15166c398ba2332e60aeb513d79
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-13 13:37:07 +0300

    Added task2611

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task26/task2611/Producer.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task26.task2611;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by arseniy.barsegyan on 13.07.2017.
+ */
+public class Producer implements Runnable {
+    private ConcurrentHashMap<String, String> map;
+
+    public Producer(ConcurrentHashMap map) {
+        this.map = map;
+    }
+
+    @Override
+    public void run() {
+        try {
+            int i = 0;
+            while (true) {
+                i++;
+                Thread.sleep(500);
+                map.put(String.valueOf(i), String.format("Some text for %s", String.valueOf(i)));
+            }
+
+        } catch (InterruptedException e) {
+            System.out.println(String.format("[%s] thread was terminated", Thread.currentThread().getName()));
+        }
+    }
+}
