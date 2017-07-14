20900184af186e996413ff65444efd3142cf73f4
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-14 14:23:35 +0300

    Added Chat_02 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -1,7 +1,40 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by arseniy.barsegyan on 14.07.2017.
  */
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        String line;
+        for (;;) {
+            try {
+                line = reader.readLine();
+                return line;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        int number;
+        for (;;) {
+            try {
+                number = Integer.parseInt(readString());
+                return number;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+    }
 }
