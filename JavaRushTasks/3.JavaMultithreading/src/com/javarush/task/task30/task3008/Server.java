a6362dbda6e5a27413a4d53ef20d3f131aa2872d
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-14 16:44:46 +0300

    Added Chat_07 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -3,17 +3,30 @@ package com.javarush.task.task30.task3008;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by arseniy.barsegyan on 14.07.2017.
  */
 public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<>();
 
     private static class Handler extends Thread {
         private Socket socket;
 
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message) {
+        for (Connection connection: connectionMap.values()) {
+            try {
+                connection.send(message);
+            } catch (IOException e) {
+                ConsoleHelper.writeMessage("Ошибка при отправке сообщения");
+            }
         }
     }
 
