c832aa671291def3220af90b4ca40832e4c867f9
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-15 13:00:39 +0300

    Added Chat_13 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/Client.java
@@ -1,5 +1,10 @@
 package com.javarush.task.task30.task3008.client;
 import com.javarush.task.task30.task3008.Connection;
+import com.javarush.task.task30.task3008.ConsoleHelper;
+import com.javarush.task.task30.task3008.Message;
+import com.javarush.task.task30.task3008.MessageType;
+
+import java.io.IOException;
 
 /**
  * Created by Administrator on 15.07.2017.
@@ -9,5 +14,37 @@ public class Client {
     private volatile boolean clientConnected = false;
 
     public class SocketThread extends Thread {
+    }
+
+    protected String getServerAddress() {
+        ConsoleHelper.writeMessage("Укажите адрес сервера");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() {
+        ConsoleHelper.writeMessage("Укажите номер порта");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() {
+        ConsoleHelper.writeMessage("Укажите ваше имя");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Возникло исключение");
+            clientConnected = false;
+        }
     }
 }
