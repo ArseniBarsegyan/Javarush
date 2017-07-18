f9888dfe27348b1d8707e3a4dcdc20a8977fbd9f
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-18 11:06:38 +0300

    Added Archiver_03 task

---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/ConsoleHelper.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by arseniy.barsegyan on 18.07.2017.
+ */
+public class ConsoleHelper {
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            return reader.readLine();
+        }
+    }
+
+    public static int readInt() throws IOException {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            return Integer.parseInt(reader.readLine());
+        }
+    }
+}
---
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Operation.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task31.task3110;
+
+/**
+ * Created by arseniy.barsegyan on 18.07.2017.
+ */
+public enum Operation {
+    CREATE,
+    ADD,
+    REMOVE,
+    EXTRACT,
+    CONTENT,
+    EXIT
+}
