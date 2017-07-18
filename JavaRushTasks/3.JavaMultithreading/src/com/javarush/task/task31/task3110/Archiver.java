8c79eafe634d2a50602709bd823d463edc61a5f2
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-18 13:58:19 +0300

    Added Archiver_08 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task31.task3110;
 
 import com.javarush.task.task31.task3110.command.ExitCommand;
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,7 +13,43 @@ import java.nio.file.Paths;
  * Created by arseniy.barsegyan on 18.07.2017.
  */
 public class Archiver {
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.valueOf(Operation.CREATE.ordinal()) + " - упаковать файлы в архив");
+        ConsoleHelper.writeMessage(String.valueOf(Operation.ADD.ordinal()) + " - добавить файл в архив");
+        ConsoleHelper.writeMessage(String.valueOf(Operation.REMOVE.ordinal()) + " - удалить файл из архива");
+        ConsoleHelper.writeMessage(String.valueOf(Operation.EXTRACT.ordinal()) + " - распаковать архив");
+        ConsoleHelper.writeMessage(String.valueOf(Operation.CONTENT.ordinal()) + " - просмотреть содержимое архива");
+        ConsoleHelper.writeMessage(String.valueOf(Operation.EXIT.ordinal()) + " – выход");
+        int number = ConsoleHelper.readInt();
+        Operation operation = null;
+        for (Operation op: Operation.values()) {
+            if (op.ordinal() == number) {
+                operation = op;
+            }
+        }
+        return operation;
+    }
+
     public static void main(String[] args) {
+        Operation operation = null;
+        while (true) {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+                break;
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+                break;
+            }
+            if (operation == Operation.EXIT) {
+                break;
+            }
+        }
+
+
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Enter full source file path");
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/PathIsNotFoundException.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/exception/PathIsNotFoundException.java
@@ -1,7 +1,4 @@
 package com.javarush.task.task31.task3110.exception;
 
-/**
- * Created by arseniy.barsegyan on 18.07.2017.
- */
 public class PathIsNotFoundException extends Exception {
 }
