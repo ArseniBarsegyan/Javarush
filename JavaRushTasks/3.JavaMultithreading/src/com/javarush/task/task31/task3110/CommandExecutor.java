54d0e343402745b0bda6ed14c17e7f77f25def81
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-18 13:23:48 +0300

    Added Archiver_07 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/CommandExecutor.java
@@ -5,31 +5,22 @@ import com.javarush.task.task31.task3110.command.*;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by arseniy.barsegyan on 18.07.2017.
- */
 public class CommandExecutor {
     private static final Map<Operation, Command> allKnownCommandsMap = new HashMap<>();
 
     static {
-        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
-        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
         allKnownCommandsMap.put(Operation.CREATE, new ZipCreateCommand());
-        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
-        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.ADD, new ZipAddCommand());
         allKnownCommandsMap.put(Operation.REMOVE, new ZipRemoveCommand());
+        allKnownCommandsMap.put(Operation.EXTRACT, new ZipExtractCommand());
+        allKnownCommandsMap.put(Operation.CONTENT, new ZipContentCommand());
+        allKnownCommandsMap.put(Operation.EXIT, new ExitCommand());
     }
 
     private CommandExecutor() {
-
     }
 
     public static void execute(Operation operation) throws Exception {
-        for (Map.Entry<Operation, Command> pair : allKnownCommandsMap.entrySet()) {
-            if (operation.equals(pair.getKey())) {
-                Command command = pair.getValue();
-                command.execute();
-            }
-        }
+        allKnownCommandsMap.get(operation).execute();
     }
 }
