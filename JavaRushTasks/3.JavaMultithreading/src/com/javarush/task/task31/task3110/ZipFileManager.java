decdb38d1145ae1df79cf94b76b071c6d2a4ef1d
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-18 10:56:40 +0300

    Added Archiver_02 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by arseniy.barsegyan on 18.07.2017.
@@ -13,6 +17,18 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        try(ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile));
+            InputStream inputStream = Files.newInputStream(source))
+        {
+            ZipEntry entry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(entry);
 
+            while (inputStream.available() > 0) {
+                byte[] buffer = new byte[inputStream.available()];
+                inputStream.read(buffer);
+                zipOutputStream.write(buffer);
+            }
+            zipOutputStream.closeEntry();
+        }
     }
 }
