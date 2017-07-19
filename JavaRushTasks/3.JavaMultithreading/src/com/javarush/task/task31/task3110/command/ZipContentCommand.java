1d6c20254d8a0ef1132cb9b5cc40533e1ed6bc0e
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-19 19:21:47 +0300

    Added Archiver_14 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipContentCommand.java
@@ -1,11 +1,26 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.FileProperties;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.util.List;
+
 /**
  * Created by arseniy.barsegyan on 18.07.2017.
  */
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
 
+        ZipFileManager manager = getZipFileManager();
+        ConsoleHelper.writeMessage("Содержимое архива:");
+
+        List<FileProperties> propertiesList = manager.getFilesList();
+        for (FileProperties prop : propertiesList) {
+            ConsoleHelper.writeMessage(prop.toString());
+        }
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
