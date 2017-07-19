ed1c2a476736110eee7b32289e2ca3a8c8c6769d
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-19 13:43:28 +0300

    Added Archiver_12 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -7,19 +7,20 @@ import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/**
- * Created by arseniy.barsegyan on 18.07.2017.
- */
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
         try {
             ConsoleHelper.writeMessage("Создание архива.");
-            ZipFileManager manager = getZipFileManager();
-            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации");
-            Path path = Paths.get(ConsoleHelper.readString());
-            manager.createZip(path);
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(sourcePath);
+
             ConsoleHelper.writeMessage("Архив создан.");
+
         } catch (PathIsNotFoundException e) {
             ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
         }
