4c886406073e6b9a4ae86105ca5c40c241959c6a
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-07-19 08:52:20 +0300

    Added Archiver_11 task

--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,7 +1,19 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by arseniy.barsegyan on 18.07.2017.
  */
 public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь архива файла");
+        String path = ConsoleHelper.readString();
+        Path path1 = Paths.get(path);
+        return new ZipFileManager(path1);
+    }
 }
--- JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
+++ JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCreateCommand.java
@@ -1,11 +1,27 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by arseniy.barsegyan on 18.07.2017.
  */
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
-
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+            ZipFileManager manager = getZipFileManager();
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для архивации");
+            Path path = Paths.get(ConsoleHelper.readString());
+            manager.createZip(path);
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
