f94a62f3abcd54bd30363b21fdeb9916c960c534
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-08-22 14:39:35 +0300

    Added task3112 - download file from url and save it at selected folder

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task31/task3112/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task31.task3112;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/* 
+Загрузчик файлов
+*/
+public class Solution {
+
+    public static void main(String[] args) throws IOException {
+        Path passwords = downloadFile("https://www.amigo.com/ship/secretPassword.txt", Paths.get("D:/MyDownloads"));
+
+        for (String line : Files.readAllLines(passwords)) {
+            System.out.println(line);
+        }
+    }
+
+    public static Path downloadFile(String urlString, Path downloadDirectory) throws IOException {
+        URL url = new URL(urlString);
+        InputStream inputStream = url.openStream();
+
+        Path tempFile = Files.createTempFile("temp-", ".tmp");
+        Files.copy(inputStream, tempFile);
+        inputStream.close();
+
+        String fileName = urlString.substring(urlString.lastIndexOf('/'));
+        Path resultFile = Paths.get(downloadDirectory.toString() + fileName);
+
+        Files.move(tempFile, resultFile);
+        return Paths.get(downloadDirectory.toString() + urlString.substring(urlString.lastIndexOf("/")));
+    }
+}
