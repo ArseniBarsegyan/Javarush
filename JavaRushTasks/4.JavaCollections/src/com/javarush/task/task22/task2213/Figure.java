0d67fba51f262bec5d862ee653ce774ff1201f3b
Arseni.Barsegyan <Arseni.Barsegyan@gmail.com>
2017-08-29 15:18:36 +0300

    Added Tetris_01 task. Added classes: Field, Figure, Tetris (with main())

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * Created by arseniy.barsegyan on 29.08.2017.
+ */
+public class Figure {
+}
---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * Created by arseniy.barsegyan on 29.08.2017.
+ */
+public class Tetris {
+    public static void main(String[] args) {
+
+    }
+}
