e23ba9cf01977385e7cfcbede3a5179de4b9799f
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-09-28 22:53:14 +0300

    Added task3313 - JSON serialize date

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task33/task3313/Event.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task33.task3313;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.util.Date;
+
+@JsonAutoDetect
+public class Event {
+    @JsonProperty
+    public String name;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    public Date eventDate;
+
+    public Event(String name) {
+        this.name = name;
+        eventDate = new Date();
+    }
+}
