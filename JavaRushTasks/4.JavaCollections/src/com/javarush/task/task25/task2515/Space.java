7efa04a9fab5099601ad7e7d94d270f1ecbfd7b8
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-10-04 22:19:24 +0300

    Added Space_01 task. Added classes SpaceShip, Ufo, Space (main)

---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * Created by Administrator on 04.10.2017.
+ */
+public class Space {
+    public static void main(String[] args){
+
+    }
+}
---
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task25.task2515;
+
+/**
+ * Created by Administrator on 04.10.2017.
+ */
+public class SpaceShip {
+}
