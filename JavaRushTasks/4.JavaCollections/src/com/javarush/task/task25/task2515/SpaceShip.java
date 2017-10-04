1146723bb733b262c739c7a216111dc18660ec90
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-10-04 22:32:44 +0300

    Added Space_05 task. BaseObject class updated. Added fields x,y,radius, isAlive

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -4,4 +4,7 @@ package com.javarush.task.task25.task2515;
  * Created by Administrator on 04.10.2017.
  */
 public class SpaceShip extends BaseObject {
+    public SpaceShip(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
