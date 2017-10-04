f57b36a2bedd4e5f0e9aecdd3b811b0f1229b5bf
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-10-04 23:27:51 +0300

    Added Space_08 task. Canvas class updated. added setPoint() method fill point with symbol 'c'. drawMatrix() - call setPoint() method

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -25,4 +25,24 @@ public class Canvas {
     public char[][] getMatrix() {
         return matrix;
     }
+
+    public void setPoint(double x, double y, char c) {
+        if (x < 0 || y < 0 || y >= matrix.length || x >= matrix[0].length) {
+
+        } else {
+            int x1 = (int)Math.round(x);
+            int y1 = (int)Math.round(y);
+            matrix[y1][x1] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x+j, y+i, c);
+                }
+            }
+        }
+    }
 }
