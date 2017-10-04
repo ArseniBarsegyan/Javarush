d5d53c9f374e8768cfb93d9bfb8fb4ef1296f2e9
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-10-04 22:27:24 +0300

    Added task Space_03. Added fields to Space class (collections of ufos, rockets, bombs, one ship and space width and height)

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Space.java
@@ -1,9 +1,54 @@
 package com.javarush.task.task25.task2515;
 
+import java.util.ArrayList;
+
 /**
  * Created by Administrator on 04.10.2017.
  */
 public class Space {
+    private int width;
+    private int height;
+    private SpaceShip ship;
+    private ArrayList<Ufo> ufos;
+    private ArrayList<Rocket> rockets;
+    private ArrayList<Bomb> bombs;
+
+    public Space(int width, int height){
+        this.width = width;
+        this.height = height;
+        ufos = new ArrayList<>();
+        rockets = new ArrayList<>();
+        bombs = new ArrayList<>();
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public SpaceShip getShip() {
+        return ship;
+    }
+
+    public void setShip(SpaceShip ship) {
+        this.ship = ship;
+    }
+
+    public ArrayList<Ufo> getUfos() {
+        return ufos;
+    }
+
+    public ArrayList<Rocket> getRockets() {
+        return rockets;
+    }
+
+    public ArrayList<Bomb> getBombs() {
+        return bombs;
+    }
+
     public static void main(String[] args){
 
     }
