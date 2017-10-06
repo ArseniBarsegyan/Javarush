8dc174b2e1fa6e43aa1e313afa3b70c499510e50
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-10-06 16:38:52 +0300

    Added Space_14 task. Ufo class updated. added draw(), move(), fire() methods.

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -1,10 +1,37 @@
 package com.javarush.task.task25.task2515;
 
+import java.util.Random;
+
 /**
  * Created by Administrator on 04.10.2017.
  */
 public class Ufo extends BaseObject {
-    public Ufo(double x, double y, double radius) {
-        super(x, y, radius);
+    private static int[][] matrix = {
+            {1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 0, 0, 0, 1},
+            {1, 1, 1, 1, 1},
+    };
+
+    public Ufo(double x, double y) {
+        super(x, y, 3);
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+    public void move() {
+        double dx = Math.random() * 2 - 1;
+        double dy = Math.random() * 2 - 1;
+        this.x += dx;
+        this.y += dy;
+        checkBorders(radius, Space.game.getWidth() - radius + 1, y, Space.game.getHeight()/2);
+        if (new Random().nextInt(10) == 1) {
+            fire();
+        }
+    }
+    public void fire() {
+        Space.game.getBombs().add(new Bomb(x, y+3));
     }
 }
