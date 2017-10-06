7c0868535f2884b3a89ea6aa696a0940164a455a
Arseni Barsegyan <Arseni.Barsegyan@gmail.com>
2017-10-06 16:12:53 +0300

    Added Space_12 task. SpaceShip class updated. Added methods moveLeft() and moveRight() - changes dx to -1 and 1 value

--- JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
+++ JavaRushTasks/4.JavaCollections/src/com/javarush/task/task25/task2515/Rocket.java
@@ -1,17 +1,26 @@
 package com.javarush.task.task25.task2515;
 
 /**
- * Created by Administrator on 04.10.2017.
+ * Класс для объектов-ракета
  */
 public class Rocket extends BaseObject {
+
     public Rocket(double x, double y) {
         super(x, y, 1);
     }
 
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    @Override
     public void draw(Canvas canvas) {
-        canvas.setPoint(x,y,'R');
+        canvas.setPoint(x, y, 'R');
     }
 
+    /**
+     * Двигаем себя вверх на один ход.
+     */
+    @Override
     public void move() {
         y--;
     }
